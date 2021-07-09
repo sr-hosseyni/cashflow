65578ab4476953ac24c953f1e0027e032b2e66b8
Rassoul Hosseini <rassoul.hosseini@gmail.com>
2021-07-10 02:48:45 +0300

    Replace App nampespace with Cashflow

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Cashflow;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
